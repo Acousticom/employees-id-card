3111ad8863e4b268dc3d96c3d47dcaaa00317181
Acousticom <omkarsrinivasa.2000@gmail.com>
2024-01-31 16:39:01 +0530

    added: page not found

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="">
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </div>
   );
